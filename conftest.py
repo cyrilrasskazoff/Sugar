641aba62694195afb6c67fc2f0e0ffbfffab0613
cyrilrasskazoff <cyrilrasskazoff@gmail.com>
2024-01-27 16:15:32 +0300

    test

--- conftest.py
+++ conftest.py
@@ -1,11 +1,15 @@
 import pytest
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.firefox.options import Options as OptionsFirefox
 
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default="chrome",
+    parser.addoption('--browser_name', action='store', default="firefox",
                      help="Choose browser: chrome or firefox")
 
 
@@ -16,11 +20,13 @@ def browser(request):
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
         options = Options()
-        browser = webdriver.Chrome(options=options)
+        service = Service(executable_path=ChromeDriverManager().install())
+        browser = webdriver.Chrome(options=options, service=service)
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
         options_firefox = OptionsFirefox()
-        browser = webdriver.Firefox(options=options_firefox)
+        service = Service(executable_path=GeckoDriverManager().install())
+        browser = webdriver.Firefox(options=options_firefox, service=service)
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
