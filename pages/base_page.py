476078b61c0157f3fe813cd66c1d900bcc78c5cc
cyrilrasskazoff <cyrilrasskazoff@gmail.com>
2024-02-04 17:13:35 +0300

    base_page

--- pages/base_page.py
+++ pages/base_page.py
@@ -3,30 +3,30 @@ from selenium.common.exceptions import NoAlertPresentException
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from .locators import BasePageLocators
 
 
 class BasePage:
     def __init__(self, driver, url, timeout=10):
-        self.browser = driver
+        self.driver = driver
         self.url = url
         self.driver.implicitly_wait(timeout)
 
     def open(self):
         self.driver.get(self.url)
 
-    # def is_element_present(self, method, element):
-    #     try:
-    #         self.driver.find_element(method, element)
-    #     except NoSuchElementException:
-    #         return False
-    #     return True
+    def is_optional_element_present(self, method, element, timeout=10):
+        try:
+            WebDriverWait(self.driver, timeout).until(
+                EC.visibility_of_element_located((method, element)))
+        except (NoSuchElementException, TimeoutException):
+            return False
+        pass
 
     def is_element_present(self, method, element, timeout=10):
         try:
             WebDriverWait(self.driver, timeout).until(
                 EC.visibility_of_element_located((method, element)))
-        except NoSuchElementException:
+        except (NoSuchElementException, TimeoutException):
             return False
         return True
 
@@ -36,7 +36,6 @@ class BasePage:
             WebDriverWait(self.driver, timeout).until(EC.presence_of_element_located((method, element)))
         except TimeoutException:
             return True
-
         return False
 
     def is_disappeared(self, method, element, timeout=10):  # метод, который проверяет, что элемент исчезнет со страницы по
@@ -47,16 +46,6 @@ class BasePage:
             # WebDriver ждёт 10 секунды и делает запросы каждую секунду
         except TimeoutException:
             return False
-
         return True
 
-    def go_to_login_page(self):  # метод, который будет проверять переход по ссылке на страницу логина
-        login_link = self.driver.find_element(
-            *BasePageLocators.LOGIN_LINK)  # * указывает на то, что мы передали именно пару, и этот кортеж нужно
-        # распаковать.
-        login_link.click()
-
-    def should_be_login_link(self):
-        assert self.is_element_present(
-            *BasePageLocators.LOGIN_LINK), "Login link is not presented"
 
