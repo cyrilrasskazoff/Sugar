ff481b37cb9a399773a8bcfb5912a485026c6f05
cyrilrasskazoff <cyrilrasskazoff@gmail.com>
2024-02-04 17:14:42 +0300

    locators

--- pages/locators.py
+++ pages/locators.py
@@ -2,7 +2,20 @@ from selenium.webdriver.common.by import By
 
 
 class LoginPageLocators:
-    LOGIN_FORM = (By.CLASS_NAME, "loginForm__PjCel")
-    EMAIL = (By.NAME, "email")
-    PASSWORD = (By.NAME, "password")
-    LOGIN_BUTTON = (By.TAG_NAME, "button")
+    LOGIN_FORM = (By.CSS_SELECTOR, "[name ='login']")
+    USERNAME = (By.CSS_SELECTOR, "[id = 'username']")
+    PASSWORD = (By.CSS_SELECTOR, "[id = 'password']")
+    LOGIN_BUTTON = (By.CSS_SELECTOR, "[id='submit_btn']")
+    FIRSTLOGIN_FORM = (By.CSS_SELECTOR, "[class='firstlogin-title consent-firstlogin-title']")
+    ACCEPT_CHECKBOX = (By.CSS_SELECTOR, "[type='checkbox']")
+    CONTINUE_BTN = (By.CSS_SELECTOR, "[name='continue_button']")
+    USER_INFO_FORM = (By.XPATH, "//span[text()='Setup your user information']")
+    NEXT_BUTTON = (By.CSS_SELECTOR, "[name='next_button']")
+    TIME_DATE_FORM = (By.XPATH, "//span[text()= 'Set your time zone, date, and name formats']")
+    START_BUTTON = (By.CSS_SELECTOR, "[name='start_sugar_button']")
+    STARTSUGAR_FORM = (By.XPATH, "//span[text()= 'Start using Sugar!']")
+
+
+class HomePageLocators:
+    SUGAR_LOGO = (By.CSS_SELECTOR, "[title='SugarCRM']")
+
