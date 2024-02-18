7480bbbf8b5f3e31fa92a232c2fd577eddba09cc
cyrilrasskazoff <cyrilrasskazoff@gmail.com>
2024-02-18 18:39:19 +0300

    test_accounts

---
+++ test_accounts_page.py
@@ -0,0 +1,13 @@
+from .pages.login_page import LoginPage
+from .pages.home_page import HomePage
+from .pages.accounts_page import AccountsPage
+from .pages.variables import URL
+
+
+def test_admin_user_is_navigated_to_accounts_page_after_click_on_accounts(driver_login):
+    url = URL
+    page = HomePage(driver_login, url)
+    page.accounts_link_navigates_to_accounts_page()
+    page = AccountsPage(driver_login, driver_login.current_url)
+    page.should_be_accounts_page()
+
